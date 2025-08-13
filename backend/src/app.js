f64e6de6071e119594e54a286c811674110b59a0
Awani Patel <162141309+Awanipatel@users.noreply.github.com>
2025-08-13 23:13:56 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,14 +21,13 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/api/v1/users", userRoutes);
 
-const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
-
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
-    });
+const start = async() =>{
+    app.set("mongo_user", "your_mongo_user");
+    const connectionDb = await mongoose.connect("mongodb+srv://awani:9kLPpQJMsxauQsST@cluster0.hqtj4bw.mongodb.net/");
+    console.log(`MONGO Connected DB host:${connectionDb.connection.host}`)
+    server.listen(app.get("port"), ()=>{
+        console.log("Server is running on port 8000");
+    })
 
 
 
